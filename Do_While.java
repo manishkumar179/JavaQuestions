59b2de71571ebf794fc9d630015070258ea6bff2
Manish Kumar <192117778+manishkumar179@users.noreply.github.com>
2025-03-12 16:49:08 +0530

    PRINT HELLO USING DO WHILE UNTIL WRONG INPUT

---
+++ Do_While.java
@@ -0,0 +1,10 @@
+public class Do_While {
+    public static void main(String[] args) {
+        int c=1;
+        do{
+            System.out.println("Hello");
+            c++;
+        }while(c<=5);
+    }
+    
+}
