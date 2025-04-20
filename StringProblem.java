b015227b0d8df36999caf37b5c8a79d57832d137
Manish Kumar <192117778+manishkumar179@users.noreply.github.com>
2025-04-20 21:39:12 +0530

    Question on string

---
+++ StringProblem.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class StringProblem {
+    public static void main(String[] args) {
+        String s="Manish";
+
+        //size of an array
+        System.out.println("Length of string = " + s.length());
+
+        //convert string into charArray
+        char[] s1=s.toCharArray();
+
+        //print the element 
+        System.out.println(Arrays.toString(s1));
+
+
+        
+        
+        
+    }
+}
