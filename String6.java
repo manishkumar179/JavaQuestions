495db11dd7b6e29d42e31c723ed529fae209e521
Manish Kumar <192117778+manishkumar179@users.noreply.github.com>
2025-05-01 11:52:02 +0530

    Integer value of alphabet like A , Z , a , z

---
+++ String6.java
@@ -0,0 +1,13 @@
+public class String6 {
+    public static void main(String[] args) {
+        int ch1='A';
+        int ch2='Z';
+        int ch3='a';
+        int ch4='z';
+        System.out.println(ch1 + " "+ ch2+" "+ ch3+" "+ch4);
+        System.out.println("Sum of char A and Z is : " + ( ch1+ch2));
+
+        int ch5='M';
+        System.out.println(ch5);
+    }
+}
