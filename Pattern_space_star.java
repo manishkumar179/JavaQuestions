1ba537d0ce0e1f95c220416343bd277d94e3661d
Manish Kumar <mk0437271@gmail.com>
2026-08-23 21:03:37 +0530

    Print pattern of space and star

---
+++ Pattern_space_star.java
@@ -0,0 +1,22 @@
+/**
+ * Pattern_space_star
+ */
+public class Pattern_space_star {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+            
+            for (int i = 1; i <= (n-row) ; i++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= n ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
